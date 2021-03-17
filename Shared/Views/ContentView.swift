158a0e4b8e08548902ba8a2992e51c97f2af6981
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-03-17 12:28:10 -0400

    separated list view into multiple composable views, made them update live so that tasks can be checked and unchecked

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -13,31 +13,11 @@ struct ContentView: View {
     
     var body: some View {
         List(store.tasks) {task in
-            HStack {
-                Image(systemName: task.completed ? "checkmark.circle.fill" : "circle")
-                    .onTapGesture {
-                        print("State of list BEFORE toggle")
-                        for (index, task) in store.tasks.enumerated() {
-                            print("Task at \(index) has completed value of \(task.completed)")
-                        }
-                        print("About to toggle")
-                        
-                        task.completed.toggle()
-                        
-                        print("just toggled")
-                        // Current state of the list:
-                        print("State of list AFTER toggle")
-                        for (index, task) in store.tasks.enumerated() {
-                            print("Task at \(index) has completed value of \(task.completed)")
-                        }
-                    }
-                
-                
-                Text(task.description)
-            }
+            TaskCell(task: task)
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Shared/Views/TaskCell.swift
@@ -0,0 +1,31 @@
+//
+//  TaskCell.swift
+//  Reminders (iOS)
+//
+//  Created by Harris-Stoertz, Rowan on 2021-03-17.
+//
+
+import SwiftUI
+
+struct TaskCell: View {
+    
+    @ObservedObject var task: Task
+    
+    var body: some View {
+        HStack {
+            Image(systemName: task.completed ? "checkmark.circle.fill" : "circle")
+                .onTapGesture {
+                    
+                    task.completed.toggle()
+                    
+                    }
+            Text(task.description)
+                }
+    }
+}
+
+struct TaskCell_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskCell(task: testData[0])
+    }
+}
